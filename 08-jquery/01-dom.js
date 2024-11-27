6d252c939f056a93d34702efc42931bf9f9fad65
Puji97 <110811680+Puji97@users.noreply.github.com>
2024-11-27 13:29:21 -0800

    07 and 08 completed with checklist and reflection

--- 08-jquery/01-dom.js
+++ 08-jquery/01-dom.js
@@ -1,48 +1,35 @@
 // Exercise #1:
 // When the user clicks the 'copy' button, copy the user input to the output area
 
-// Option #1
-// Fetch JavaScript objects representing specific elements in the DOM
-let userInput1 = document.querySelector('#userInput1');
-let copy = document.querySelector('#copy');
-let output1 = document.querySelector('#output1');
+// let userInput1 = document.querySelector("#userInput1");
+// let copy = document.querySelector("#copy");
+// let output1 = document.querySelector("#output1");
 
-// Add an event listener on the target element
-copy.addEventListener('click', handleClick);
+// copy.addEventListener("click", handleClick);
 
-// Callback function to handle event
-function handleClick(event) {
-  console.log('click event', event);
-  output1.textContent = userInput1.value;
-}
+// function handleClick(event) {
+//   console.log("click event", event);
+//   output1.textContent = userInput1.value;
+// }
+$(document).ready(() => {
+  $("#copy").on("click", (event) => {
+    $("#output1").text($("#userInput1").val());
+  });
 
-// Option #2
-// document.getElementById('copy').onclick = () => {
-//   let userInput = document.getElementById('userInput1');
-//   let output = document.getElementById('output');
+  // Exercise #2:
+  // When the user enters input text, copy the user input to the output area
 
-//   output.textContent = userInput.value;
-// };
+  // let userInput2 = document.querySelector("#userInput2");
+  // let output2 = document.querySelector("#output2");
 
-// Option #3
-// document.getElementById('copy').onclick = () => {
-//   document.getElementById('output').textContent = document.getElementById(
-//     'userInput1'
-//   ).value;
-// };
+  // userInput2.addEventListener("input", handleInput);
 
-// Exercise #2:
-// When the user enters input text, copy the user input to the output area
+  // function handleInput(event) {
+  //   console.log("click event", event);
+  //   output2.textContent = userInput2.value;
+  // }
 
-// Fetch JavaScript objects representing specific elements in the DOM
-let userInput2 = document.querySelector('#userInput2');
-let output2 = document.querySelector('#output2');
-
-// Add an event listener on the target element
-userInput2.addEventListener('input', handleInput);
-
-// Callback function to handle event
-function handleInput(event) {
-  console.log('click event', event);
-  output2.textContent = userInput2.value;
-}
+  $("#userInput2").on("input", (event) => {
+    $("#output2").text($("#userInput2").val());
+  });
+});
